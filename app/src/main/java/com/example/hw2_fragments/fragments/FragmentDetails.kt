3a755627cdc3d15863d6119b1d176526def5323f
Pavel Shakin <pashkin302@gmail.com>
2021-11-13 21:46:21 +0200

    Added recyclerView usage to FragmentDetails

--- app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
+++ app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
@@ -3,12 +3,20 @@ package com.example.hw2_fragments.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hw2_fragments.R
+import com.example.hw2_fragments.adapter.UserAdapter
+import com.example.hw2_fragments.data.User
+import com.example.hw2_fragments.data.UsersData
 
 class FragmentDetails : Fragment(R.layout.fragment_details) {
 
+    private lateinit var users: List<User>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        users = UsersData(requireActivity().application).usersList
+        val recyclerView: RecyclerView? = getView()?.findViewById(R.id.recyclerView)
+        recyclerView!!.adapter = UserAdapter(users)
     }
 }
