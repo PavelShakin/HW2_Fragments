2d63a1253a40be581f25f766930db13afab6b4fa
Pavel Shakin <pashkin302@gmail.com>
2021-11-13 17:09:50 +0200

    Created two fragments

---
+++ app/src/main/java/com/example/hw2_fragments/fragments/FragmentButtons.kt
@@ -0,0 +1,14 @@
+package com.example.hw2_fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.hw2_fragments.R
+
+class FragmentButtons : Fragment(R.layout.fragment_buttons) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
@@ -0,0 +1,14 @@
+package com.example.hw2_fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.hw2_fragments.R
+
+class FragmentDetails : Fragment(R.layout.fragment_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
