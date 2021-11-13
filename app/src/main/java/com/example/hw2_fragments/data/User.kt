01f0ff8aa190948d08b885be96c16edfb96cb857
Pavel Shakin <pashkin302@gmail.com>
2021-11-13 17:07:14 +0200

    Created User and added list of users

---
+++ app/src/main/java/com/example/hw2_fragments/data/User.kt
@@ -0,0 +1,12 @@
+package com.example.hw2_fragments.data
+
+data class User(
+
+    val userId: Int,
+    var name: String,
+    val profilePhoto: String,
+    val wasOnline: String,
+    var hobby: String,
+    var email: String,
+    var status: String
+)
