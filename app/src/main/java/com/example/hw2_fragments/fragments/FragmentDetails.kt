17894ed63e0529df195763a35d7acc0a89e83076
Pavel Shakin <pashkin302@gmail.com>
2021-11-15 18:42:52 +0200

    Added viewModel

--- app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
+++ app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
@@ -1,22 +1,31 @@
 package com.example.hw2_fragments.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hw2_fragments.R
 import com.example.hw2_fragments.adapter.UserAdapter
 import com.example.hw2_fragments.data.User
 import com.example.hw2_fragments.data.UsersData
+import com.example.hw2_fragments.view_model.SharedViewModel
+import java.util.*
 
 class FragmentDetails : Fragment(R.layout.fragment_details) {
 
-    private lateinit var users: List<User>
+    private lateinit var users: LinkedList<User>
+    private lateinit var viewModel: SharedViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        users = UsersData(requireActivity().application).usersList
         val recyclerView: RecyclerView? = getView()?.findViewById(R.id.recyclerView)
-        recyclerView!!.adapter = UserAdapter(users)
+
+        viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        viewModel.getUsers().observe(viewLifecycleOwner, {
+            users = viewModel.getUsers().value!!
+            recyclerView!!.adapter = UserAdapter(users)
+        })
     }
 }
