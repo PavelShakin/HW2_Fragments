44812c47a307e2c7fb50a889e3c5b55cc1a441ea
Pavel Shakin <pashkin302@gmail.com>
2021-11-15 18:57:27 +0200

    Added uploading all users to recycler by default

--- app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
+++ app/src/main/java/com/example/hw2_fragments/fragments/FragmentDetails.kt
@@ -1,7 +1,6 @@
 package com.example.hw2_fragments.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -9,7 +8,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.hw2_fragments.R
 import com.example.hw2_fragments.adapter.UserAdapter
 import com.example.hw2_fragments.data.User
-import com.example.hw2_fragments.data.UsersData
 import com.example.hw2_fragments.view_model.SharedViewModel
 import java.util.*
 
@@ -23,6 +21,7 @@ class FragmentDetails : Fragment(R.layout.fragment_details) {
         val recyclerView: RecyclerView? = getView()?.findViewById(R.id.recyclerView)
 
         viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        viewModel.uploadDefaultUsersList()
         viewModel.getUsers().observe(viewLifecycleOwner, {
             users = viewModel.getUsers().value!!
             recyclerView!!.adapter = UserAdapter(users)
